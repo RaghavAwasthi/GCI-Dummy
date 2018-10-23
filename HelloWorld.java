26d3b8151d8cd18928afd15492c81b4697997403
Raghav Awasthi <raghavawasthi2014@gmail.com>
2018-10-23 22:23:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,14 @@
+/* The folowing file has been made as a part of a task in GCI 2018
+
+*/
+// Welcome to GCI 2018
 
 
-The folowing file has been made as a part of a task in GCI 2018 
-
-
-public class HelloWorld
+public class HelloWorld // An important Note the file NAme and class Name should be exactly same
 {
-public static void main(String ar[])
+public static void main(String ar[]) // used to pass commandline paramaters which can be accessed through the array indexes
 {
+  System.out.println(" GCI 2018 is amazing " )
 System.out.println("Hello World");
 }
 }
