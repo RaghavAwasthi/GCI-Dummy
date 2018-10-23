fe58c4c0856f89875131393b68347e04415bc7fa
Raghav Awasthi <raghavawasthi2014@gmail.com>
2018-10-23 22:10:47 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,13 @@
 
 
-The folowing file has been made as a part of a task in GCI 2018 
-
+//The folowing file has been made as a part of a task in GCI 2018 
+import java.util.*;
 
 public class HelloWorld
 {
 public static void main(String ar[])
-{
+{ Thread t=this;
+ t.sleep(1500);
 System.out.println("Hello World");
 }
 }
