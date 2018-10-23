2ee9a467bf39b3729e81e3d234dc55e3dbceba61
Raghav Awasthi <raghavawasthi2014@gmail.com>
2018-10-23 22:02:15 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+
+The folowing file has been made as a part of a task in GCI 2018 
+
+
+public class HelloWorld
+{
+public static void main(String ar[])
+{
+System.out.println("Hello World");
+}
+}
+// Welcome to GCI 2018
